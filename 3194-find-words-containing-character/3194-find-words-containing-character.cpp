ee573c45842ee9f9d194000eede947d96ea9efa5
Bhavika Puppalwar <125676992+Bhavikaa324@users.noreply.github.com>
2025-05-15 22:41:44 +0530

    Time: 1 ms (35.93%), Space: 33 MB (80.81%) - LeetHub

---
+++ 3194-find-words-containing-character/3194-find-words-containing-character.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> idx;
+        for (int i = 0; i < words.size(); i++) {
+            for (char ch : words[i]) {
+                if (ch == x) {
+                    idx.push_back(i);
+                    break;
+                }
+            }
+        }
+        return idx;
+    }
+};
