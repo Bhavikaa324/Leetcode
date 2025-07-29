9ef00e63cf90973d0194227466fc2767d2235b94
Bhavika Puppalwar <125676992+Bhavikaa324@users.noreply.github.com>
2025-07-29 16:58:01 +0530

    Time: 0 ms (100%), Space: 10.3 MB (85.14%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void backtrack(int index, vector<int>& nums, vector<int>& temp, vector<vector<int>>& result) {
+        result.push_back(temp);
+        for (int i = index; i < nums.size(); i++) {
+            if (i > index && nums[i] == nums[i - 1]) continue;
+            temp.push_back(nums[i]);
+            backtrack(i + 1, nums, temp, result);
+            temp.pop_back(); 
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> temp;
+        sort(nums.begin(), nums.end()); 
+        backtrack(0, nums, temp, result);
+        return result;
+    }
+    
+};
