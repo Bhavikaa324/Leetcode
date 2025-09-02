7a37d71370887cbcd3188439a77d4f7d242b7dfe
Bhavika Puppalwar <125676992+Bhavikaa324@users.noreply.github.com>
2025-09-02 19:10:10 +0530

    Time: 253 ms (12.1%), Space: 29.2 MB (8.52%) - LeetHub

---
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int n=s.length();
+        int ans=0;
+        for (int i=0;i<n;i++) {
+        map<char, int> mpp; 
+        for (int j=i;j<n;j++) {
+            mpp[s[j]]++; 
+        int mini=INT_MAX,maxi=0;
+        for(auto it:mpp){
+            mini=min(mini,it.second);
+            maxi=max(maxi,it.second);
+        }
+         ans += (maxi-mini);
+        }}
+        return ans;
+    }
+};
