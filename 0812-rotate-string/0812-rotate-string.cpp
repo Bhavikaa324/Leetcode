63d0dad7121fb1177361f11fd9a120d7922bf0ca
Bhavika Puppalwar <125676992+Bhavikaa324@users.noreply.github.com>
2025-09-06 01:26:33 +0530

    Time: 0 ms (100%), Space: 8.3 MB (25.05%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         int n =s.length();
-        for (int k = 0; k < n; k++) {
-    reverse(s.begin(), s.begin() + 1);   
-    reverse(s.begin() + 1, s.end());  
-    reverse(s.begin(), s.end());     
 
-    if (s == goal) return true; 
+    for(int k=0;k<n;k++){
+        string temp = s;
+            reverse(temp.begin(), temp.begin() + k);   
+            reverse(temp.begin() + k, temp.end());  
+            reverse(temp.begin(), temp.end());   
+
+    if (temp == goal) return true; 
 }return false;
     }
 };
